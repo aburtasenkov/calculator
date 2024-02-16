f50bdd570f22230a9c1ab978c996aa71a7a97a10
aburtasenkov <59296596+aburtasenkov@users.noreply.github.com>
2024-02-16 18:59:55 +0100

    Update Roman.h

--- Roman.h
+++ Roman.h
@@ -13,15 +13,15 @@ namespace Roman {
 		int integer;
 	};
 
-	const vector<std::string> roman_numeral_tbl{ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-	const vector<int> roman_number_tbl{ 1000, 900, 500, 400,  100,  90,  50,   40,  10,   9,   5,    4,    1 };
+	vector<std::string> roman_numeral_tbl{ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+	vector<int> roman_number_tbl{ 1000, 900, 500, 400,  100,  90,  50,   40,  10,   9,   5,    4,    1 };
 
-	std::string roman_numeral(Roman_int r)
+	std::string get_roman_numeral(Roman_int r)
 	{
 		int number = r.as_int();
 		std::string roman;
 
-		for (int index = 0; index < roman_numeral_tbl.size(); ++index)
+		for (int index = 0; index < Roman::roman_numeral_tbl.size(); ++index)
 		{
 			while (number - roman_number_tbl[index] >= 0)
 			{
@@ -33,9 +33,36 @@ namespace Roman {
 		return roman;
 	}
 
+	int value(char roman)
+	{
+		switch (roman)
+		{
+		case 'I': return 1;
+		case 'V': return 5;
+		case 'X': return 10;
+		case 'L': return 50;
+		case 'C': return 100;
+		case 'D': return 500;
+		case 'M': return 1000;
+		}
+	}
+
+	Roman_int string_to_roman(std::string s)
+	{
+		int sum = 0;
+		int last = 0;
+		for (int index = s.length() - 1; index >= 0; --index)
+		{
+			if (value(s[index]) >= last) sum += value(s[index]);
+			else sum -= value(s[index]);
+			last = value(s[index]);
+		}
+		return sum;
+	}
+
 	std::ostream& operator<<(std::ostream& os, Roman_int& r)
 	{
-		return os << roman_numeral(r);
+		return os << get_roman_numeral(r);
 	}
 
 	std::istream& operator>>(std::istream& is, Roman_int& r)
